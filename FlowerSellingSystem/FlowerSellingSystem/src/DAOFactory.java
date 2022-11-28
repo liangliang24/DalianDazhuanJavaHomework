322976a5c7b845fb4177e89d94289a14e858a822
liangliang24 <2794515623@qq.com>
2022-11-28 10:04:01 +0800

    DAOFactory

--- FlowerSellingSystem/FlowerSellingSystem/src/DAOFactory.java
+++ FlowerSellingSystem/FlowerSellingSystem/src/DAOFactory.java
@@ -1,7 +1,28 @@
 public class DAOFactory
 {
-    public static DAOInterface getInstance(String instance)
+    public DAOFactory()
     {
         
+    }
+
+    public static DAOInterface getInstance(String instance)
+    {
+        if (instance == "Flower")
+        {
+            return new FlowerDAO();
+        }
+        else if (instance == "Customer")
+        {
+            return new CustomerDAO();
+        }
+        else if (instance == "FlowerStore")
+        {
+            return new FlowerStoreDAO();
+        }
+        else if (instance == "Orders")
+        {
+            return new OrdersDAO();
+        }
+        return null;
     }
 }
