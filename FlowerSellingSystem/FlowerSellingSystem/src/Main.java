b67a03bc793a361780f0a82e8bbe7931595d3d71
liangliang24 <2794515623@qq.com>
2022-12-02 16:47:55 +0800

    Final Finished

--- FlowerSellingSystem/FlowerSellingSystem/src/Main.java
+++ FlowerSellingSystem/FlowerSellingSystem/src/Main.java
@@ -6,7 +6,7 @@ public class Main
     public static void main(String[] args)
     {
 
-        FlowerStoreTable frame = new FlowerStoreTable(new User("FlowerStore"));
+        LoginTable frame = new LoginTable();
         frame.setVisible(true);
     }
 
